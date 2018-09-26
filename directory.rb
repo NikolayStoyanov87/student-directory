1efe271da78080e28af179fa1f7487ef28e954cb
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-26 09:09:25 +0100

    Array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
+#lets put all students into an array.
+students = [
+  "Dr. Hanibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Curlione",
+  "Alex Delaege",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+#and then print them
 puts "The students of Villains academy"
 puts "--------------------------------"
-puts "Dr. Hanibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Curlione"
-puts "Alex Delaege"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Finally ,we print the total number of students.
-# it's importants that print doesn't add new line characters.
-puts "Overaall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+#finally we print the total
+puts "Overaall, we have #{students.count} great students"
