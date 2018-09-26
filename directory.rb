e627ca0f300a9caaf3ead6d3a4190b14dd6a3829
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-26 14:10:57 +0100

    Nested array

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
 #lets put all students into an array.
 students = [
-  "Dr. Hanibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Curlione",
-  "Alex Delaege",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hanibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Curlione", :november],
+  ["Alex Delaege", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 #and then print them
 def print_header
   puts "The students of Villains academy"
   puts "--------------------------------"
 end
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
-def print_footer(name)
-  puts "Overaall, we have #{name.count} great students"
+def print_footer(students)
+  puts "Overaall, we have #{students.count} great students"
 end
 #nothing happens untill we call the methods.
 print_header
