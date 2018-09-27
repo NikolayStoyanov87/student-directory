67ab5d6a1d11fee5cb05ec8fdca705492c9bff8b
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-27 11:16:32 +0100

    adding more info into the student hash

--- directory.rb
+++ directory.rb
@@ -1,17 +1,13 @@
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Uk, height: "4ft" },
+  {name: "Darth Vader", cohort: :november, hobbies: :sport_climbing, country_of_birth: :Argentina, height: "5ft"},
+  {name: "Nurse Ratched", cohort: :november, hobbies: :big_wall_climbing, country_of_birth: :Germany, height: "6ft"},
+  {name: "Michael Corleone", cohort: :november, hobbies: :ice_climbing, country_of_birth: :Uk, height: "6ft"},
+  {name: "Alex DeLarge", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Spain, height: "7ft"},
+  {name: "The Wicked Witch of the West", cohort: :november, hobbies: :hiking, country_of_birth: :Bulgaria, height: "6ft"},
+  {name: "Terminator", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Portugal, height: "4ft"},
+  {name: "Freddy Krueger", cohort: :november, hobbies: :football, country_of_birth: :Greece, height: "5ft"},
+  {name: "The Joker", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Uk, height: "5ft"},
+  {name: "Joffrey Baratheon", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Croatia, height: "6ft"},
+  {name: "Norman Bates", cohort: :november, hobbies: :hockey, country_of_birth: :Iceland, height: "6ft"}
 ]
-while students.length != 0 do
- puts students
-break
-end
