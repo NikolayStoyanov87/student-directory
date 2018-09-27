d00a3606e7fba622dc8ffcda5d63cfa49671e04a
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-27 13:00:38 +0100

    method center() of the String

--- directory.rb
+++ directory.rb
@@ -1,13 +1,34 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Uk, height: "4ft" },
-  {name: "Darth Vader", cohort: :november, hobbies: :sport_climbing, country_of_birth: :Argentina, height: "5ft"},
-  {name: "Nurse Ratched", cohort: :november, hobbies: :big_wall_climbing, country_of_birth: :Germany, height: "6ft"},
-  {name: "Michael Corleone", cohort: :november, hobbies: :ice_climbing, country_of_birth: :Uk, height: "6ft"},
-  {name: "Alex DeLarge", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Spain, height: "7ft"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobbies: :hiking, country_of_birth: :Bulgaria, height: "6ft"},
-  {name: "Terminator", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Portugal, height: "4ft"},
-  {name: "Freddy Krueger", cohort: :november, hobbies: :football, country_of_birth: :Greece, height: "5ft"},
-  {name: "The Joker", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Uk, height: "5ft"},
-  {name: "Joffrey Baratheon", cohort: :november, hobbies: :indoor_climbing, country_of_birth: :Croatia, height: "6ft"},
-  {name: "Norman Bates", cohort: :november, hobbies: :hockey, country_of_birth: :Iceland, height: "6ft"}
-]
+def print_header
+  puts "The students of Villains Academy".center(80)
+  puts "--------------------------------".center(80)
+end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(80)
+  end
+end
+def input_students
+  puts "Please enter the name of the students".center(80)
+  puts "To finish just hit return twice".center(80)
+  #create an ampty array.
+  students = []
+  #get the first name.
+  name = gets.chomp
+  #while the name is not empty repeate this code.
+  while !name.empty? do
+    #add the student hash into an array.
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students".center(80)
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+def print_footer(students)
+  puts "Overaall, we have #{students.count} great students".center(80)
+end
+students = input_students
+print_header
+print(students)
+print_footer(students)
