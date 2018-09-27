308dc62ac8e13de60849e1abbb89ddf34a98cc6b
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-27 09:39:55 +0100

    Array_each_with_index_starting_from_no_1

--- directory.rb
+++ directory.rb
@@ -1,34 +1,14 @@
-def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def input_students
-  puts "Please enter the name of the students"
-  puts "To finish just hit return twice"
-  #create an ampty array.
-  students = []
-  #get the first name.
-  name = gets.chomp
-  #while the name is not empty repeate this code.
-  while !name.empty? do
-    #add the student hash into an array.
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end
-  #return the array of students
-  students
-end
-def print_footer(students)
-  puts "Overaall, we have #{students.count} great students"
-end
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
+students.each_with_index{|value, index| puts "#{index + 1}. #{value}."}
