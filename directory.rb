8e5f9d9db3dfdf6e0cc54784cbcdc5b9bccf8b4d
NikolayStoyanov87 <bushon87@gmail.com>
2018-09-29 09:15:11 +0100

    Find all typos exercise

--- directory.rb
+++ directory.rb
@@ -1,53 +1,38 @@
-def print_header
-  puts "The students of Villains Academy".center(80)
-  puts "--------------------------------".center(80)
-end
-def print(students)
-  categorised = students.group_by { |person| person[:cohort] }
-  categorised.each do |cohort_type, members|
-  puts cohort_type
-  members.each { |member| puts member[:name] }
-  end
-end
 def input_students
-  puts "Please enter the name of the students".center(80)
-  puts "What's your cohort?".center(80)
-  puts "To finish just hit return twice".center(80)
-  #create an ampty array.
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
   students = []
-  #get the first name.
+  # get the first name
   name = gets.chomp
-    if name == ""
-      puts "add student name".center(80)
-      name = gets.chomp
-    end
-  cohort = gets.chomp
-    if cohort == ""
-      cohort = :october #setting a default value if input is nothing
-    end
-  #while the name is not empty repeate this code.
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    #add the student hash into an array.
-    students << {name: name, cohort: cohort.to_sym}
-    if students.length > 1
-      puts "Now we have #{students.count} students".center(80)
-    else
-      puts "Now we have #{students.count} student".center(80)
-    end
-    #get another name from the user
+    # add the student hash to the array
+    students << {name: name.to_sym, cohort: :cohort}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
     name = gets.chomp
-    cohort = gets.chomp
   end
-  #return the array of students
+  # return the array of students
   students
 end
-def print_footer(students)
-  if students.length > 1
-    puts "Overall, we have #{students.count} great students".center(80)
-  else
-    puts "Overall, we have #{students.count} great student".center(80)
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------------------------------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+
 students = input_students
 print_header
 print(students)
